113f0638803a362f66f0cd89d7b9bac40d5e2db6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-11-19 18:25:24 +0100

    Add some analysis. Not working yet.

--- sample_analysis.sh
+++ sample_analysis.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# current script location. Blessed be SO…
+ME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # check some old vr theater files, just for sport
 FIRST_COMMIT=`git rev-list --max-parents=0 HEAD`
 LAST_COMMIT=19dec9277689c6a82e376b6c7fcb2b42a9a14fae
 WATCHED_FILES=assets
 
-GIT_HISTORY=`mktemp -t git_history_XXXX.csv`
+GIT_HISTORY=`mktemp -t git_history_XXXX.txt`
 CLOC_RESULT=`mktemp -t cloc_XXXX.csv`
+CODEMAT_RESULTS=`mktemp -t codemaat_XXXX.csv`
 
 echo "* writing git output to ${GIT_HISTORY}…"
 git log --pretty=format:'[%h] "%an" %ad %s' --date=short --numstat ${FIRST_COMMIT}..${LAST_COMMIT} -- ${WATCHED_FILES}  >${GIT_HISTORY}
+
+echo "* extracting the salient bits with code maat in ${CODEMAT_RESULTS}"
+java -jar ${ME_DIR}/code-maat-*-standalone.jar -c git -a ${CODEMAT_RESULTS} -l ${GIT_HISTORY}
 
 echo "* checking the commit out…"
 git checkout ${LAST_COMMIT}
@@ -18,3 +25,4 @@ echo "* cloc-ing the code in ${CLOC_RESULT}…"
 cloc --by-file --csv --quiet ${WATCHED_FILES} >${CLOC_RESULT}
 
 echo "* let's print some codemat basic statistics…"
+python ${ME_DIR}/scripts\ 4/merge_comp_freqs.py ${CODEMAT_RESULTS} ${CLOC_RESULT}
