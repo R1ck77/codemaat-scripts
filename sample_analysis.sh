123d4ee484f91a7e534225ea40e28db3370c97a4
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-11-19 18:52:18 +0100

    Script that doesn't work, but should
    The python script doesn't seem to work right :p

--- sample_analysis.sh
+++ sample_analysis.sh
@@ -2,6 +2,7 @@
 
 # current script location. Blessed be SO…
 ME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CODE_MAAT="java -jar ${ME_DIR}/code-maat-*-standalone.jar"
 
 # check some old vr theater files, just for sport
 FIRST_COMMIT=`git rev-list --max-parents=0 HEAD`
@@ -16,13 +17,13 @@ echo "* writing git output to ${GIT_HISTORY}…"
 git log --pretty=format:'[%h] "%an" %ad %s' --date=short --numstat ${FIRST_COMMIT}..${LAST_COMMIT} -- ${WATCHED_FILES}  >${GIT_HISTORY}
 
 echo "* extracting the salient bits with code maat in ${CODEMAT_RESULTS}"
-java -jar ${ME_DIR}/code-maat-*-standalone.jar -c git -a ${CODEMAT_RESULTS} -l ${GIT_HISTORY}
+${CODE_MAAT} -c git -a revisions -l ${GIT_HISTORY} >${CODEMAT_RESULTS}
 
 echo "* checking the commit out…"
 git checkout ${LAST_COMMIT}
 
 echo "* cloc-ing the code in ${CLOC_RESULT}…"
-cloc --by-file --csv --quiet ${WATCHED_FILES} >${CLOC_RESULT}
+cloc --by-file --csv --quiet ${WATCHED_FILES} --report-file=${CLOC_RESULT}
 
 echo "* let's print some codemat basic statistics…"
 python ${ME_DIR}/scripts\ 4/merge_comp_freqs.py ${CODEMAT_RESULTS} ${CLOC_RESULT}
