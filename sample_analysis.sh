38e5994363b03436293a73c82b1791d051bdc7de
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-11-26 18:13:48 +0100

    Minor changes to the script

--- sample_analysis.sh
+++ sample_analysis.sh
@@ -9,9 +9,6 @@ ME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CODE_MAAT="java -jar ${ME_DIR}/code-maat-*-standalone.jar"
 JSON_CONVERSION_DIR="${ME_DIR}/json-conversion"
 
-# check some old vr theater files, just for sport
-WATCHED_FILES=.
-
 if [ $NORMALIZATION -lt 1 ] 
 then
     echo "Automatic normalization…"
@@ -26,7 +23,7 @@ MERGED_RESULTS=`mktemp -t merged_XXXX.csv`
 JSON_RESULTS=${MERGED_RESULTS}.json
 
 echo "* writing git output to ${GIT_HISTORY}…"
-git log --pretty=format:'[%h] "%an" %ad %s' --date=short --numstat ${FIRST_COMMIT}..${LAST_COMMIT} -- ${WATCHED_FILES}  >${GIT_HISTORY}
+git log --pretty=format:'[%h] "%an" %ad %s' --date=short --numstat ${FIRST_COMMIT}..${LAST_COMMIT}  >${GIT_HISTORY}
 
 echo "* printing some basic updates:"
 ${CODE_MAAT} -c git -a summary -l ${GIT_HISTORY}
@@ -38,12 +35,14 @@ echo "* checking the commit out…"
 git checkout ${LAST_COMMIT}
 
 echo "* cloc-ing the code in ${CLOC_RESULT}…"
-cloc --by-file --csv --quiet ${WATCHED_FILES} --report-file=${CLOC_RESULT}
+cloc --by-file --csv --quiet . --report-file=${CLOC_RESULT}
 
 echo "* merging the complexity and changes into ${MERGED_RESULTS}…"
-python ${ME_DIR}/scripts\ 4/merge_comp_freqs.py ${CODEMAT_RESULTS} ${CLOC_RESULT} >${MERGED_RESULTS}
+pushd ${JSON_CONVERSION_DIR}
+lein run merge ${CODEMAT_RESULTS} ${CLOC_RESULT} >${MERGED_RESULTS}
+popd
 
 echo "* converting the complexity into json…"
-pushd $JSON_CONVERSION_DIR
-lein run ${MERGED_RESULTS} ${NORMALIZATION} >${JSON_RESULTS}
+pushd ${JSON_CONVERSION_DIR}
+lein run jsonify ${MERGED_RESULTS} ${NORMALIZATION} >${JSON_RESULTS}
 popd
